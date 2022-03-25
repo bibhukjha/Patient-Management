4bf483d3c2a14c2e6969483c0de74df2bc831284
bibhukjha <msgbox.ubibhu@gmail.com>
2022-03-25 16:32:21 +0530

    Menu driven program

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect('Hospital.db')
+conn = sqlite3.connect('Hospitals.db')
 c = conn.cursor()
 
 # 1 Adding the patients details into table
 '''
-c.execute("""CREATE TABLE patients_mang(patCode int,
+c.execute("""CREATE TABLE patients_mangm(patCode int PRIMARY KEY,
                  name text,
                  phone int,
                  addCity text) """)
 print("Table Created")
 '''
 
-many_patients = [(1, 'Rohan Patil', 12345678, 'Pune'),
-                 (19, 'Sameer Rao', 12345679, 'Mumbai'),
-                 (109, 'Peter Parker', 98676532, 'Nasik'),
-                 ]
-c.executemany("INSERT INTO patients_mang VALUES(?,?,?,?)", many_patients)
-# print("Executed Successfully")
 
-c.execute("SELECT * FROM patients_mang")
+insert_query = """INSERT INTO patients_mangm (patCode,name,phone,addCity) VALUES (1,'Rohan gavaskar', 12345678, 'Pune'),
+                 (19, 'Sachin Tendulkar', 12345679, 'Mumbai'),
+                 (109, 'Peter Parker', 98676532, 'Nasik')"""
+c.execute(insert_query)
+print('Data of the Patient table are:')
+c.execute(''' SELECT rowid,* FROM patients_mangm ''')
 items = c.fetchall()
 for item in items:
-    print(items)
+    print(item)
 conn.commit()
 conn.close()
